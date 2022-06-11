5bb39790cf65f65a6e249ee9f3dc678c47b32e81
Administrator <Administrator@PC>
2022-06-11 08:22:51 +0200

    CHANGELOG
    #SplashScreen -> Añadido una SplashScreen que muestra el logo de la aplicacion durante un par de segundos.
    #CleanCode -> Automatic

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using ELROOMBA.Viewmodels;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Threading.Tasks;
 
 
 // To learn more about WinUI, the WinUI project structure,
@@ -31,11 +36,66 @@ public partial class App : Application
     /// will be used such as when the application is launched to open a specific file.
     /// </summary>
     /// <param name="args">Details about the launch request and process.</param>
-    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
-        _window = new MainWindow();
+        /// <summary>
+        /// Logo de la SplashScreen
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        Image Logo = new Image();
+        Logo.Source = new BitmapImage(new Uri("ms-appx:///Assets/ElRoomba.png"));
+        Logo.Width = 100;
+        Logo.Height = 100;
+        Logo.VerticalAlignment = VerticalAlignment.Center;
+        Logo.HorizontalAlignment = HorizontalAlignment.Center;
+
+        /// <summary>
+        /// Texto de la SplashScreen
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        TextBlock LogoTxt = new TextBlock();
+        LogoTxt.Text = "ElRoomba";
+        LogoTxt.VerticalAlignment = VerticalAlignment.Center;
+        LogoTxt.HorizontalAlignment = HorizontalAlignment.Center;
+        LogoTxt.Margin = new Thickness(0, 145, 0, 0);
+        LogoTxt.FontWeight = FontWeights.Bold;
+
+        /// <summary>
+        /// Barra de progreso de la aplicacion
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        ProgressBar cargando = new ProgressBar();
+        cargando.Width = 75;
+        cargando.Height = 75;
+        cargando.IsIndeterminate = true;
+        cargando.HorizontalAlignment = HorizontalAlignment.Center;
+        cargando.VerticalAlignment = VerticalAlignment.Center;
+        cargando.Margin = new Thickness(0, 245, 0 , 0);
+
+        /// <summary>
+        /// Grid necesario para el orden de los componentes
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        Grid grid = new Grid();
+        grid.Children.Add(Logo);
+        grid.Children.Add(cargando);
+        grid.Children.Add(LogoTxt);
+
+        /// <summary>
+        /// Mostramos la ventana y preguardamos el estado incial de la 
+        /// ventana en una variable,Mostramos la SplashScreen el tiempo deseado
+        /// y Por supuesto despues  de que eso acabe, volvemos a cargar el contenido
+        /// preguardado anteriormente en la variable.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        _window = new Window();
+        Mwindow = _window.Content;
+        _window.Content = grid;
         _window.Activate();
+        await Task.Delay(2000);
+        _window.Content = (UIElement)Mwindow;
     }
 
     private Window? _window;
+    private object Mwindow;
 }
