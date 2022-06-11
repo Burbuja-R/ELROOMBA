9e5ff216db24d88186f9537ccb8f5e940a3c21ba
Administrator <Administrator@PC>
2022-06-11 07:49:07 +0200

    Agregar archivos de proyecto.

---
+++ Viewmodels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ELROOMBA.Viewmodels;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class MainWindowViewModel
+{
+
+
+}
